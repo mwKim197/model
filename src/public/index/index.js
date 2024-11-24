4411995dbe60f4c785408469c359d170f347c959
peropero502321 <peropero50@naver.com>
2024-11-24 19:46:11 +0900

    구조변경

--- src/public/index/index.js
+++ src/public/index/index.js
@@ -3,6 +3,13 @@ const log = require('../../logger');
 
 const { ipcRenderer } = require('electron');
 
+function sendLogToMain(level, message) {
+    ipcRenderer.send('log-to-main', { level, message });
+}
+
+sendLogToMain('info', '렌더러에서 보내는 정보 로그');
+sendLogToMain('error', '렌더러 에러 발생');
+
 // 페이지 이동 버튼
 document.getElementById('goToAdmin').addEventListener('click', () => {
     ipcRenderer.send('navigate-to-page', 'admin'); // 'admin' 페이지로 이동
