832324b7ae44ae5f238cafd8579470a64b90f0da
peropero502321 <peropero50@naver.com>
2024-11-28 21:20:41 +0900

    자동 업데이트 적용

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow} = require('electron');
+const { autoUpdater } = require("electron-updater");
 const path = require('path');
 const express = require('express');
 const http = require('http');
@@ -88,6 +89,43 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
+/* Updater ======================================================*/
+
+autoUpdater.on('checking-for-update', () => {
+    log.info('업데이트 확인 중...');
+});
+autoUpdater.on('update-available', (info) => {
+    log.info('업데이트가 가능합니다.');
+});
+autoUpdater.on('update-not-available', (info) => {
+    log.info('현재 최신버전입니다.');
+});
+autoUpdater.on('error', (err) => {
+    log.info('에러가 발생하였습니다. 에러내용 : ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - 현재 ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    log.info(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+    log.info('업데이트가 완료되었습니다.');
+});
+
+/* Electron =====================================================*/
+
+/** 초기화가 끝나게 되면 실행 */
+app.on('ready', () => {
+    // 메인 창 생성
+    createWindow();
+
+    // 자동 업데이트 등록
+    autoUpdater.checkForUpdates();
+});
+
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
