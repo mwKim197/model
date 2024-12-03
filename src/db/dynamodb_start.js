1eb364b495579f1d1ef35ffda92b8b4e22973f2f
peropero502321 <peropero50@naver.com>
2024-12-03 19:42:38 +0900

    소스수정

--- src/db/dynamodb_start.js
+++ src/db/dynamodb_start.js
@@ -1,10 +1,13 @@
-require('dotenv').config();  // .env 파일에서 환경 변수 로드
 const AWS = require('aws-sdk');
+const log = require('../logger');
 
 // 환경 변수에서 자격 증명 값 읽어오기
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.AWS_DEFAULT_REGION;
+const accessKeyId = process.env.MODEL_AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.MODEL_AWS_SECRET_ACCESS_KEY;
+const region = process.env.MODEL_AWS_DEFAULT_REGION;
+
+log.info(accessKeyId);
+log.info(region);
 
 // AWS 자격 증명 설정
 AWS.config.update({
