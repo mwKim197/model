1d8948038528dea32b474e0d83f0ffbe9d23c63e
peropero502321 <peropero50@naver.com>
2024-12-10 22:55:04 +0900

    아이스수정

--- src/util/numberConvert.js
+++ src/util/numberConvert.js
@@ -1,9 +1,9 @@
 function convertTimeToHex(timeInSeconds) {
-    const multipliedValue = Math.round(timeInSeconds * 10);  // 초를 10배로 변환
-    const hexValue = multipliedValue.toString(16).toUpperCase();  // 16진수로 변환
-    return hexValue.padStart(2, "0");  // 두 자리가 되도록 패딩
+    const multipliedValue = Math.round(timeInSeconds * 10); // 초를 10배로 변환
+    const hexValue = multipliedValue.toString(16).toUpperCase(); // 16진수로 변환
+    return `0x${hexValue.padStart(2, "0")}`; // 항상 0x로 시작하도록 반환
 }
 
 module.exports = {
-    convertTimeToHex
-}
+    convertTimeToHex,
+};
