be6e96aa5d7554979995792b000c3f9551a0e1eb
peropero502321 <peropero50@naver.com>
2024-12-12 22:07:57 +0900

    세척레시피 수정 - 컵센서 1 번만보게 수정

--- src/services/serialOrderManager.js
+++ src/services/serialOrderManager.js
@@ -458,12 +458,6 @@ const useWash = async (data) => {
     if (menuData.length > 0) {
         log.info("세척 시작...!");
 
-        // 컵 센서 체크
-        const isStopValid = await checkCupSensor("없음", 3);
-        if (!isStopValid) {
-            log.error("컵 센서 상태가 '없음'이 아니어서 세척 작업을 중단합니다.");
-            return; // 작업 중단
-        }
         const recipe = menuData.filter(menu => orderData.some(ord => ord.menuId === menu.menuId));
 
         log.info(`전체 세척 레시피: ${JSON.stringify(recipe)}!`);
@@ -474,6 +468,15 @@ const useWash = async (data) => {
         log.info(`전체 세척 레시피 리스트: ${JSON.stringify(combinedList)}`);
 
         for (let i = 0; i < combinedList.length; i++) {
+
+            if (i === 0) {
+                // 컵 센서 체크
+                const isStopValid = await checkCupSensor("없음", 3);
+                if (!isStopValid) {
+                    log.error("컵 센서 상태가 '없음'이 아니어서 세척 작업을 중단합니다.");
+                    return; // 작업 중단
+                }
+            }
             const listData = combinedList[i];
 
             log.info(`전체 세척 실행: ${JSON.stringify(listData)}`);
