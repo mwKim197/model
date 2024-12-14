6652f9c9cb9378f19e288235ee9bd7045f827130
peropero502321 <peropero50@naver.com>
2024-12-14 13:22:19 +0900

    폰트적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
-    safelist: [
-        'light-font',
-    ],
     theme: {
         extend: {
             fontFamily: {
-                custom: ['LightFont', 'sans-serif'], // 기본 폰트
+                custom: ['Light', 'sans-serif'], // 기본 폰트
             },
             colors: {
                 'menu-gray': '#666666',
