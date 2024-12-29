7e1248651dd1d8006ac7c44b80bd5cbe42e3d21e
peropero502321 <peropero50@naver.com>
2024-12-29 19:26:37 +0900

    수정 변경 버전업

--- src/services/serialOrderManager.js
+++ src/services/serialOrderManager.js
@@ -133,6 +133,11 @@ const processOrder = async (recipe) => {
                 if (index === sortedItems.length - 1) {
                     const isEndValid = await checkCupSensor("없음", 3, true);
                     if (!isEndValid) {
+                        eventEmitter.emit('order-update', {
+                            menu: recipe.name, // 수정: menuName 변수 대신 recipe.name 사용
+                            status: 'completed',
+                            message: `"120초 경과로 기계가 초기화되었습니다."`
+                        });
                         log.error(`[에러] 컵 센서 상태가 유효하지 않음 (회수 실패): menuId ${recipe.menuId}`);
                         throw new Error(`"120초 경과로 기계가 초기화되었습니다."`);
                     }
