d7642659d216cbfbc29377e9c27f7f5483ec71b1
peropero502321 <peropero50@naver.com>
2025-01-02 00:26:59 +0900

    포트포워딩 1시간 이상 유지 변경

--- src/services/portForwarding.js
+++ src/services/portForwarding.js
@@ -3,27 +3,33 @@ const log = require('../logger');
 
 function setupPortForwarding(publicPort, privatePort) {
     const client = upnp.createClient();
-    client.portMapping(
-        {
-            public: publicPort,
-            private: privatePort,
-            ttl: 3600,
-        },
-        (err) => {
-            if (err) {
-                log.error(`포트 포워딩 실패 (공용 포트: ${publicPort}, 사설 포트: ${privatePort}):`, err.message);
-                log.error('디버깅 정보:', {
-                    stack: err.stack,
-                    code: err.code,
-                });
-            } else {
-                log.info(`포트 포워딩 성공: 외부 포트 ${publicPort} → 내부 포트 ${privatePort}`);
-            }
-        }
-    );
 
-    // 포트 해제
+    function mapPort() {
+        client.portMapping(
+            {
+                public: publicPort,
+                private: privatePort,
+                ttl: 3600, // TTL 1시간
+            },
+            (err) => {
+                if (err) {
+                    log.error(`포트 포워딩 실패 (공용 포트: ${publicPort}, 사설 포트: ${privatePort}):`, err.message);
+                } else {
+                    log.info(`포트 포워딩 성공: 외부 포트 ${publicPort} → 내부 포트 ${privatePort}`);
+                }
+            }
+        );
+    }
+
+    // 처음 포트 매핑
+    mapPort();
+
+    // 55분(3300초)마다 포트 매핑 갱신
+    const interval = setInterval(mapPort, 3300 * 1000);
+
+    // 프로그램 종료 시 포트 해제
     process.on('exit', () => {
+        clearInterval(interval);
         client.portUnmapping(
             { public: publicPort },
             (err) => {
